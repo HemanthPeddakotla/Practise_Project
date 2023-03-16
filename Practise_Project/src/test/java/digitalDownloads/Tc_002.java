eeb1787f723547db73274f39d08f48734416116a
Hemanth <Hemanth@LAPTOP-TVJ0FV6G>
2023-03-16 12:42:05 +0530

    Provided code for second TestScript

--- Practise_Project/src/test/java/digitalDownloads/Tc_002.java
+++ Practise_Project/src/test/java/digitalDownloads/Tc_002.java
@@ -1,5 +1,24 @@
 package digitalDownloads;
 
-public class Tc_002 {
+import java.util.List;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import generic_utility.BaseUnit;
+import pom_repo.DigitalDownloadPage;
+
+public class Tc_002 extends BaseUnit{
+	@Test
+	public void dig() {
+		DigitalDownloadPage page = new DigitalDownloadPage(driver);
+		page.getDigiDownloadsCategory().click();
+		page.getSortByDropDown().click();
+		Select select = new Select(page.getSortByDropDown());
+		List<WebElement> menu = select.getOptions();
+		for(WebElement ele : menu) {
+			System.out.println(ele.getText());
+		}
+	}
 }
